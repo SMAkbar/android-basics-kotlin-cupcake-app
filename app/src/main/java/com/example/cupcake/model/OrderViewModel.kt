1b8a8ae9c191913aa3f4871ffed469206611a0eb
smakbar <sm.akbar42@gmail.com>
2021-10-08 17:17:10 +0500

    Completed the integration of the interpolation

---
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+class OrderViewModel : ViewModel() {
+    init {
+        resetOrder()
+    }
+
+    private val _quantity = MutableLiveData<Int>()
+    val quantity : LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData<String>()
+    val flavor : LiveData<String> = _flavor
+
+    private val _date = MutableLiveData<String>()
+    val date : LiveData<String> = _date
+
+    private val _price = MutableLiveData<Double>()
+    val price : LiveData<Double> = _price
+
+    fun setQuantity(numberCupcakes: Int) {
+        _quantity.value = numberCupcakes
+    }
+
+    fun setFlavor(desiredFlavor: String) {
+        _flavor.value = desiredFlavor
+    }
+
+    val dateOptions = getPickupOptions()
+
+    fun setDate(pickupDate: String) {
+        _date.value = pickupDate
+    }
+
+    fun hasNoFlavorSet(): Boolean {
+        return _flavor.value.isNullOrEmpty()
+    }
+
+    private fun getPickupOptions() : List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calender = Calendar.getInstance()
+        repeat(4) {
+            options.add(formatter.format(calender.time))
+            calender.add(Calendar.DATE, 1)
+        }
+        return options
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+}
